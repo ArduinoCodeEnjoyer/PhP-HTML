2ccaf16c7483055824f2bbcd3ccba84aabe0007d
ArduinoCodeEnjoyer <6521061001@cdti.ac.th>
2024-01-22 12:21:19 +0700

    Update 2

--- insert_smartfarm.php
+++ insert_smartfarm.php
@@ -2,10 +2,10 @@
 
 include "connect.php";
 
-$zone = $_GET['zone'];
+$zone = $_POST['zone'];
 
 $sql = "INSERT INTO tbl_smartfarm_iot (zone, board, temp, humid, dmy)
-VALUES ('$zone', '$_GET[board]', '$_GET[temp]', '$_GET[humid]', now())";
+VALUES ('$zone', '$_POST[board]', '$_POST[temp]', '$_POST[humid]', now())";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
